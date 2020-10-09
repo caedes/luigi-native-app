7e68c6731f91db271ac8f70932b92b3e87f3f76b
Romain Laurent <romain.laurent@cdiscount.com>
2020-10-09 17:02:49 +0200

    chore: ignore some warning through LogBox

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import "react-native-gesture-handler";
+import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "react-native-elements";
@@ -6,6 +7,8 @@ import * as React from "react";
 
 import MainNavigator from "./MainNavigator";
 import theme from "./theme";
+
+LogBox.ignoreLogs(["Setting a timer"]);
 
 export default function App() {
   return (
